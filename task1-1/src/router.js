fe1daf0f0c704abc11ec3b88d7ddb7e56e0ce574
silviaoh <silvia.jisu.oh@gmail.com>
2022-10-28 04:44:31 +0900

    Feat: 토큰 유무에 따른 리다이렉트 처리

--- task1-1/src/router.js
+++ task1-1/src/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import CheckAccessToken from './components/CheckAccessToken';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import TodoList from './pages/TodoList';
@@ -8,9 +9,23 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Signin />} />
+        <Route
+          path="/"
+          element={
+            <CheckAccessToken>
+              <Signin />
+            </CheckAccessToken>
+          }
+        />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/todo" element={<TodoList />} />
+        <Route
+          path="/todo"
+          element={
+            <CheckAccessToken>
+              <TodoList />
+            </CheckAccessToken>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
